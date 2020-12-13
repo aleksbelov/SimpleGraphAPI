a59399d802e07b6dc40a4116e4f54c9c6724f094
aleksbelov <abelov94@gmail.com>
2020-12-14 00:54:22 +0300

    run.sh comment

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 docker build -t graph .
 docker run -d --name graph -p 8765:80 graph
 docker logs graph
+
+# example of using
+# curl -F "file=@data.csv" where_service_was_deployed/graph --output out.png
