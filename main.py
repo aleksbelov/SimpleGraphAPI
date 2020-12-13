5a829626e4bf3111cf9a8a809c8160d51a909be5
aleksbelov <abelov94@gmail.com>
2020-12-14 00:21:14 +0300

    CORS

--- main.py
+++ main.py
@@ -4,8 +4,17 @@ from starlette.responses import StreamingResponse
 import matplotlib.pyplot as plt
 import numpy as np
 import io
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.post("/graph",)
