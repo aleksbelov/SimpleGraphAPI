e002c72f94c01aa1ce9a41d59d18eecce207ce1b
aleksbelov <abelov94@gmail.com>
2020-12-13 22:45:05 +0300

    app

---
+++ main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import HTMLResponse
+from starlette.responses import StreamingResponse
+import matplotlib.pyplot as plt
+import numpy as np
+import io
+
+app = FastAPI()
+
+
+@app.post("/graph",)
+async def scatter_plot(file: UploadFile = File(...)):
+    s = await file.read()
+    data = np.genfromtxt(io.StringIO(s.decode()))
+
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4))
+    ax1.scatter(data[:, 0], data[:, 1])
+    ax2.scatter(data[:, 0]/data[:, 0].max(), data[:, 1]//data[:, 1].max())
+
+    pic_buf = io.BytesIO()
+    plt.savefig(pic_buf, format='png')
+    pic_buf.seek(0)
+
+    return StreamingResponse(pic_buf, media_type="image/png")
+
+
+@app.get("/graph", response_class=HTMLResponse)
+def upload_btn():
+    return """
+    <html>
+        <body>
+            <div align="center">
+                Use post method to send csv and get pictures
+            <div/>
+        </body>
+    </html>
+    """
